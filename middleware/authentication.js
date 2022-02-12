ce8426d5bba2275984f1839f68f4153e54174d90
mptapasdas <tapas.das.cer19@itbhu.ac.in>
2022-02-13 00:20:14 +0530

    modified some error messages

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,27 +3,27 @@ const jwt = require("jsonwebtoken");
 const { UnauthenticatedError } = require("../errors");
 
 const auth = (req, res, next) => {
-  //check header
+    //check header
 
-  const authHeader = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw new UnauthenticatedError("Token cant be authenticated");
-  }
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        throw new UnauthenticatedError("Please Login Again");
+    }
 
-  const token = authHeader.split(" ")[1];
+    const token = authHeader.split(" ")[1];
 
-  try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
-    //ATTACH THE USER TO THE JOB ROUTE
-    req.user = {
-      userId: payload.userId,
-      name: payload.name,
-    };
-    next();
-  } catch (error) {
-    throw new UnauthenticatedError("token error");
-  }
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        //ATTACH THE USER TO THE JOB ROUTE
+        req.user = {
+            userId: payload.userId,
+            name: payload.name,
+        };
+        next();
+    } catch (error) {
+        throw new UnauthenticatedError("Please Login Again");
+    }
 };
 
 module.exports = auth;
--- models/User.js
+++ models/User.js
@@ -1,59 +1,57 @@
 const mongoose = require("mongoose");
 
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
-const jwt = require('jsonwebtoken')
-
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide name"],
-    minlength: 3
-  },
-  email: {
-    type: String,
-    required: [true, "Please provide email"],
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please provide a valid email",
-    ],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide password"],
-    minlength: 6
-  },
+    name: {
+        type: String,
+        required: [true, "Please provide name"],
+        minlength: 3,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide email"],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please provide a valid email",
+        ],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Minimum password length must be 6"],
+        minlength: 6,
+    },
 });
 
-
-UserSchema.pre('save', async function(){
-  try {
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-  } catch (error) {
-    console.log('bcrypt error')
-  }
-})
+UserSchema.pre("save", async function () {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    } catch (error) {
+        console.log("bcrypt error");
+    }
+});
 
 UserSchema.methods.createJWT = function () {
-  try {
-    return jwt.sign(
-      { userId: this._id, name: this.name },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: process.env.JWT_LIFETIME,
-      }
-    )
-  } catch (error) {
-    console.log(error, 'jwt error')
-  }
-}
+    try {
+        return jwt.sign(
+            { userId: this._id, name: this.name },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: process.env.JWT_LIFETIME,
+            }
+        );
+    } catch (error) {
+        console.log(error, "jwt error");
+    }
+};
 
 UserSchema.methods.comparePassword = function (candidatePassword) {
-  const isMatch = bcrypt.compare(candidatePassword, this.password)
-  return isMatch
-}
+    const isMatch = bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+};
 
 module.exports = mongoose.model("User", UserSchema);
