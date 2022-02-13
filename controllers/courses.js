f169e59ec8a816eed68c42eb6c312068f2ce50cf
mptapasdas <tapas.das.cer19@itbhu.ac.in>
2022-02-13 22:21:50 +0530

    implemented search route

--- controllers/courses.js
+++ controllers/courses.js
@@ -12,7 +12,70 @@ const getAllCourses = async (req, res) => {
     res.status(StatusCodes.OK).json({ allCourses });
 };
 
+const editDist = (str1, str2) => {
+    let m = str1.length;
+    let n = str2.length;
+    let dp = new Array(m + 1);
+    for (let i = 0; i < m + 1; i++) {
+        dp[i] = new Array(n + 1);
+        for (let j = 0; j < n + 1; j++) {
+            dp[i][j] = 0;
+        }
+    }
+
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (i === 0) dp[i][j] = j;
+            else if (j === 0) dp[i][j] = i;
+            else if (str1[i - 1] === str2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+            else
+                dp[i][j] =
+                    1 +
+                    Math.min(
+                        dp[i][j - 1], // Insert
+                        dp[i - 1][j], // Remove
+                        dp[i - 1][j - 1]
+                    ); // Replace
+        }
+    }
+
+    return dp[m][n];
+};
+
+const searchCourses = async (req, res) => {
+    let allCourses = await Course.find({});
+    let searchValue = req.query.query;
+    searchValue.toLowerCase();
+    if (searchValue.length < 3) {
+        let filteredCourses = allCourses.filter((course) => {
+            let contains = false;
+            for (let eachTag of course.tags) {
+                contains =
+                    contains || eachTag.toLowerCase().startsWith(searchValue);
+            }
+            return contains;
+        });
+        return res.json({ filteredCourses });
+    }
+
+    let filteredCourses = allCourses.filter((course) => {
+        let contains = false;
+
+        for (let eachTag of course.tags) {
+            let newTag = eachTag
+                .toLowerCase()
+                .substring(0, Math.min(searchValue.length, eachTag.length));
+            contains = contains || editDist(searchValue, newTag) < 2;
+        }
+
+        return contains;
+    });
+
+    return res.json({ filteredCourses });
+};
+
 module.exports = {
     createCourse,
     getAllCourses,
+    searchCourses,
 };
--- routes/courses.js
+++ routes/courses.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const courseRouter = express.Router();
 
-const { getAllCourses, createCourse } = require("../controllers/courses");
+const {
+    getAllCourses,
+    createCourse,
+    searchCourses,
+} = require("../controllers/courses");
 
 courseRouter.route(`/${process.env.CREATE_COURSE_ROUTE}`).post(createCourse);
 courseRouter.route("/").get(getAllCourses);
+courseRouter.route("/search").get(searchCourses);
 
 module.exports = courseRouter;
