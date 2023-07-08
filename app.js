7c740dade6efe66ef4619849a642f7004d0615cb
mptapasdas <tapas.das.cer19@itbhu.ac.in>
2023-07-08 19:18:00 +0530

    app.js updated

--- app.js
+++ app.js
@@ -29,12 +29,12 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // extra packages
 app.set("trust proxy", 1);
-// app.use(
-//     rateLimiter({
-//         windowMs: 15 * 60 * 1000, // 15 minutes
-//         max: 100, // limit each IP to 100 requests per windowMs
-//     })
-// );
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+    })
+);
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
